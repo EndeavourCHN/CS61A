80774f05aa8c56fe8f0d80199cb158e577574e4d
EndeavourCHN <3068364094@qq.com>
2025-11-22 16:15:24 +0800

    Homework: hw08 finished

--- hws/hw08/hw08.scm
+++ hws/hw08/hw08.scm
@@ -1,7 +1,39 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+    'YOUR-CODE-HERE
+    (if (null? s)
+        #t
+        (if (null? (cdr s))
+            #t
+            (if (> (car s) (car (cdr s)))
+                #f
+                (ascending? (cdr s))
+            )
+        )
+    )
+)
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+    'YOUR-CODE-HERE
+    (cond
+        ((null? s) '())
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))
+    )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+    'YOUR-CODE-HERE
+    (cond
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) (interleave lst2 (cdr lst1))))
+    )
+)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+    'YOUR-CODE-HERE
+    (if (null? s)
+        '()
+        (cons (car s) (no-repeats (my-filter (lambda (x) (not (= x (car s)))) (cdr s))))
+    )
+)
